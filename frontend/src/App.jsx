b71556f7fdd9b8f37dcf02602a096a6ce0e7d05a
knightowlrehan <rehan@knightowl.online>
2025-11-07 10:24:13 +0530

    feat : add Stats, Mission, WhyChooseUs, CoreFeatures, HowItWorks, ReasonsToLove, Testimonials  sections to layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import Layout from './components/layout/Layout';
 import HeroSection from './components/sections/HeroSection';
+import StatsSection from './components/sections/StatsSection';
+import MissionSection from './components/sections/MissionSection';
+import WhyChooseUs from './components/sections/WhyChooseUs';
+import CoreFeatures from './components/sections/CoreFeatures';
+import HowItWorks from './components/sections/HowItWorks';
+import ReasonsToLove from './components/sections/ReasonsToLove';
+import Testimonials from './components/sections/Testimonials';
 
 function App() {
   return (
     <Layout>
       <HeroSection />
+      <StatsSection />
+      <MissionSection />
+      <WhyChooseUs/>
+      <CoreFeatures/>
+      <HowItWorks/>
+      <ReasonsToLove/>
+      <Testimonials/>
     </Layout>
   );
 }
